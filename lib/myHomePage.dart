59a03f428726b800b65f0a942fff782b1448caf8
carlostic26 <carlospenaranda26@gmail.com>
2024-04-25 15:51:33 -0500

    api consumido

--- lib/myHomePage.dart
+++ lib/myHomePage.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:consumo_api/model/gif.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -16,33 +15,37 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   late Future<List<Gif>> _listaGif;
 
-  Future<List<Gif>?> getGif() async {
+  Future<List<Gif>> getGif() async {
     final response = await http.get(Uri.parse(
         'https://api.giphy.com/v1/gifs/trending?api_key=J9cPMk2ZcVW9iYzpng3J4YPLc5itomrz&limit=10&offset=0&rating=g&bundle=messaging_non_clips'));
 
-    List<Gif> gifs = [];
+    List<Gif> gif = [];
 
     if (response.statusCode == 200) {
-      print(response.body);
+      //print(response.body);
 
       String body = utf8.decode(response.bodyBytes);
 
       //conver body to Json
       final jsonData = jsonDecode(body);
 
-      print(jsonData);
+      print(jsonData["data"]);
+
+      for (var item in jsonData["data"]) {
+        gif.add(Gif(item["title"], item["images"]["original"]["url"]));
+      }
+
+      return gif;
     } else {
       throw Exception("Fallo de conexion");
     }
-
-    return null;
   }
 
   @override
   void initState() {
     super.initState();
 
-    getGif();
+    _listaGif = getGif();
   }
 
   @override
@@ -52,25 +55,42 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      body: FutureBuilder<List<Gif>>(
+        future: _listaGif,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasError) {
+            print(snapshot.error);
+            return const Text('Error');
+          } else {
+            return ListView(
+              children: _listGifs(snapshot.data ?? []),
+            );
+          }
+        },
       ),
     );
+  }
+
+  List<Widget> _listGifs(List<Gif> data) {
+    List<Widget> gifs = [];
+
+    for (var gif in data) {
+      gifs.add(Card(
+          child: Column(
+        children: [
+          Image.network(gif.url),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(gif.name),
+          ),
+        ],
+      )));
+    }
+
+    return gifs;
   }
 }
